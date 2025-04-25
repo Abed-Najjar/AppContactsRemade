9053c5520492a06ced96a10489a7b2c562444a7f
Abed-Najjar <abdnajjar082@gmail.com>
2025-04-25 23:50:38 +0300

    Added isValidEmailAddress Attribute for validation

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f510736c151a502f8befc4a2014e96ab22e312b4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e986dc475495deca44d267f52b209e9f93288fea")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
